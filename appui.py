90ff5bd4e93212684a76e8df89d3c04bd8f87fde
arbridev <arebrif@gmail.com>
2019-12-10 09:42:24 -0400

    Refactor: View, app UI

--- appui.py
+++ appui.py
@@ -1,9 +1,22 @@
 import tkinter as tk
 
 class App(tk.Frame):
+
+    projectTitleFrm = None
+    projectLbl = None
+    tasksframe = None
+    buttonPanel = None
+    newProjectBtn = None
+    openProjectBtn = None
+    taskButtonSection = None
+    taskPanel = None
+    startTaskBtn = None
+    endTaskBtn = None
+
     def __init__(self, master=None):
         super().__init__(master)
         self.pack()
+        self.setup()
 
     def setup(self):
         root = self.master
@@ -29,3 +42,64 @@ class App(tk.Frame):
         root.geometry('%dx%d+%d+%d' % (wWidth, wHeight, x, y))
 
         root.title('Work Timekeeper')
+
+        projectTitle = 'NO PROJECT SELECTED'
+
+        self.projectTitleFrm = tk.Frame(root, bg="gray")
+        self.projectTitleFrm.place(relwidth=0.8, relheight=0.1, relx=0.2, rely=0.0)
+
+        self.projectLbl = tk.Label(self.projectTitleFrm, text=projectTitle, fg="black", bg="red")
+        self.projectLbl.pack()
+
+        self.tasksframe = tk.Frame(root, bg="white")
+        self.tasksframe.place(relwidth=0.8, relheight=0.7, relx=0.2, rely=0.1)
+
+        self.buttonPanel = tk.Frame(root, bg="gray")
+        self.buttonPanel.place(relwidth=0.2, relheight=1.0, relx=0.0, rely=0.0)
+
+        # self.newProjectBtn = tk.Button(buttonPanel, text="New Project", padx=10, pady=5, fg="black", bg="blue", command=lambda: newProject())
+        self.newProjectBtn = tk.Button(self.buttonPanel, text="New Project", padx=10, pady=5, fg="black", bg="blue")
+        self.newProjectBtn.pack()
+
+        # self.openProjectBtn = tk.Button(buttonPanel, text="Open Project", padx=10, pady=5, fg="black", bg="blue", command=lambda: openProject(projectLbl))
+        self.openProjectBtn = tk.Button(self.buttonPanel, text="Open Project", padx=10, pady=5, fg="black", bg="blue")
+        self.openProjectBtn.pack()
+
+        self.taskButtonSection = tk.Frame(self.buttonPanel, bg="gray")
+        self.taskButtonSection.place(relwidth=1.0, relheight=0.2, relx=0.0, rely=0.8)
+
+        self.taskPanel = tk.Frame(root, bg="gray", padx=10, pady=5)
+        self.taskPanel.place(relwidth=0.8, relheight=0.2, relx=0.2, rely=0.8)
+        tk.Label(self.taskPanel, text="Task:").pack()
+        self.taskEntry = tk.Text(self.taskPanel)
+        self.taskEntry.pack(side=tk.RIGHT, expand=tk.YES, fill=tk.BOTH)
+
+        # self.startTaskBtn = tk.Button(taskButtonSection, text="Start", padx=10, pady=5, fg="white", bg="#263D42", command=lambda: startTask(taskEntry))
+        self.startTaskBtn = tk.Button(self.taskButtonSection, text="Start", padx=10, pady=5, fg="white", bg="#263D42")
+        self.startTaskBtn.pack()
+
+        # self.endTaskBtn = tk.Button(taskButtonSection, text="End", padx=10, pady=5, fg="white", bg="#263D42", command=lambda: endTask(taskEntry))
+        self.endTaskBtn = tk.Button(self.taskButtonSection, text="End", padx=10, pady=5, fg="white", bg="#263D42")
+        self.endTaskBtn.pack()
+    
+    def changeProjectTitle(self, title):
+        self.projectLbl.destroy()
+        self.projectLbl = tk.Label(self.projectTitleFrm, text=title, fg="black", bg="red")
+        self.projectLbl.pack()
+
+    def setCommands(self, newProject, openProject, startTask, endTask):
+        self.newProjectBtn.destroy()
+        self.newProjectBtn = tk.Button(self.buttonPanel, text="New Project", padx=10, pady=5, fg="black", bg="blue", command=newProject)
+        self.newProjectBtn.pack()
+        
+        self.openProjectBtn.destroy()
+        self.openProjectBtn = tk.Button(self.buttonPanel, text="Open Project", padx=10, pady=5, fg="black", bg="blue", command=openProject)
+        self.openProjectBtn.pack()
+        
+        self.startTaskBtn.destroy()
+        self.startTaskBtn = tk.Button(self.taskButtonSection, text="Start", padx=10, pady=5, fg="white", bg="#263D42", command=startTask)
+        self.startTaskBtn.pack()
+        
+        self.endTaskBtn.destroy()
+        self.endTaskBtn = tk.Button(self.taskButtonSection, text="End", padx=10, pady=5, fg="white", bg="#263D42", command=endTask)
+        self.endTaskBtn.pack()
--- main.py
+++ main.py
@@ -22,7 +22,6 @@ from excel import ExcelHandler
 ## app setup
 
 app = App()
-app.setup()
 
 root = app.master
 
@@ -33,23 +32,18 @@ handler = ExcelHandler()
 def newProject():
     projectTitle = simpledialog.askstring("Input", "Project name:", parent=root)
     if projectTitle != '':
-        changeProjectTitle(projectTitle, projectLbl)
+        app.changeProjectTitle(projectTitle)
         path = os.path.join(projectsDir, projectTitle + ".xlsx")
         handler.newWorkbook(path)
         handler.inputScaffold()
 
-def openProject(projectLbl):
+def openProject():
     filename = filedialog.askopenfilename(initialdir=os.path.join(appDir, 'projects'), title="Select File", filetypes=(("excel", "*.xlsx"), ("all files", "*.*")))
     if filename != '':
-        changeProjectTitle(filename, projectLbl)
+        app.changeProjectTitle(filename)
         handler.openWorkbook(filename)
 
-def changeProjectTitle(title, projectLbl):
-    projectLbl.destroy()
-    projectLbl = tk.Label(projectTitleFrm, text=title, fg="black", bg="red")
-    projectLbl.pack()
-
-def startTask(taskEntry):
+def startTask():
     cell = handler.getNextEmptyCellDown()
     previousCell = handler.getCellAbove(cell)
     if type(previousCell.value) is int:
@@ -61,10 +55,10 @@ def startTask(taskEntry):
     cell = handler.getCellBeside(cell)
     cell.value = 'START'
     cell = handler.getCellBeside(cell)
-    cell.value = taskEntry.get("1.0", tk.END)
+    cell.value = app.taskEntry.get("1.0", tk.END)
     handler.saveWorkbook()
 
-def endTask(taskEntry):
+def endTask():
     cell = handler.getNextEmptyCellDown()
     previousCell = handler.getCellAbove(cell)
     if type(previousCell.value) is int:
@@ -76,46 +70,13 @@ def endTask(taskEntry):
     cell = handler.getCellBeside(cell)
     cell.value = 'END'
     cell = handler.getCellBeside(cell)
-    cell.value = taskEntry.get("1.0", tk.END)
+    cell.value = app.taskEntry.get("1.0", tk.END)
     handler.saveWorkbook()
 
 ## main execution
 
-projectTitle = 'NO PROJECT SELECTED'
+app.setCommands(newProject, openProject, startTask, endTask)
 
-projectTitleFrm = tk.Frame(root, bg="gray")
-projectTitleFrm.place(relwidth=0.8, relheight=0.1, relx=0.2, rely=0.0)
-
-projectLbl = tk.Label(projectTitleFrm, text=projectTitle, fg="black", bg="red")
-projectLbl.pack()
-
-tasksframe = tk.Frame(root, bg="white")
-tasksframe.place(relwidth=0.8, relheight=0.7, relx=0.2, rely=0.1)
-
-buttonPanel = tk.Frame(root, bg="gray")
-buttonPanel.place(relwidth=0.2, relheight=1.0, relx=0.0, rely=0.0)
-
-newProjectBtn = tk.Button(buttonPanel, text="New Project", padx=10, pady=5, fg="black", bg="blue", command=lambda: newProject())
-newProjectBtn.pack()
-
-openProjectBtn = tk.Button(buttonPanel, text="Open Project", padx=10, pady=5, fg="black", bg="blue", command=lambda: openProject(projectLbl))
-openProjectBtn.pack()
-
-taskButtonSection = tk.Frame(buttonPanel, bg="gray")
-taskButtonSection.place(relwidth=1.0, relheight=0.2, relx=0.0, rely=0.8)
-
-taskPanel = tk.Frame(root, bg="gray", padx=10, pady=5)
-taskPanel.place(relwidth=0.8, relheight=0.2, relx=0.2, rely=0.8)
-tk.Label(taskPanel, text="Task:").pack()
-taskEntry = tk.Text(taskPanel)
-taskEntry.pack(side=tk.RIGHT, expand=tk.YES, fill=tk.BOTH)
-
-startTaskBtn = tk.Button(taskButtonSection, text="Start", padx=10, pady=5, fg="white", bg="#263D42", command=lambda: startTask(taskEntry))
-startTaskBtn.pack()
-
-endTaskBtn = tk.Button(taskButtonSection, text="End", padx=10, pady=5, fg="white", bg="#263D42", command=lambda: endTask(taskEntry))
-endTaskBtn.pack()
-
-root.mainloop()
+app.mainloop()
 
 handler.close()
